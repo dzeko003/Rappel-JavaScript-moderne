2c7134a6c1a686877ca79e00dcf8d772d32766da
dzeko003 <berenismassamba@gmail.com>
2026-01-29 10:26:10 +0100

    Correction de la mise en forme et ajout de commentaires explicatifs pour l'affectation par décomposition des objets et des tableaux.

--- 5.Destructuring/script.js
+++ 5.Destructuring/script.js
@@ -10,42 +10,57 @@
 const userObj = {
   userName: "Karl",
   age: 44,
-  country: "Germany"
-}
+  country: "Germany",
+};
 
-
-
+// On stocke les valeurs de l'objet dans des constantes
+const { age, country, userName } = userObj;
 
 // Donner un nom différent aux constantes
 
-
+const { userName: mainName, age: exactAge, country: origin } = userObj;
 
 // Créer des constantes par défaut, au cas où les props n'existent pas.
 
+const { country, sport = "Football", beverage = "Beer" } = userObj; //les attributs sport et beverage etant inexistantes
+//  dans l'objet auront pour valeur par defaut les valeurs definit
 
+console.log(country);
 
 // Combiner un nom différent et des valeurs par défaut.
 
-
-
+const { userName: mainName = "Hanz" } = userObj;
 
 // Affecter les props d'un objet attendu en paramètre
 
+const numbers = {
+  num1: 50,
+  num2: 100,
+};
 
+function foo({ num1, num2 }) {
+  return num1 + num2;
+}
 
+console.log(foo(numbers));
 
 /* B. Affecter les valeurs d'un tableau. */
 
-const animals = ["cat","dog","mouse","lion"];
+const animals = ["cat", "dog", "mouse", "lion"];
 
-
-
+const [cat, dog] = animals; // ceci tient compte de l'ordre
 
 // valeur par défaut et ignorer des valeurs
 
+const fruits = ["strawberry", "grapes", "banana", "apple"];
 
+const [strawberry, grapes, , apple] = fruits;
 
 // affecter le reste d'un tableau
 
+const [student, ...marks] = ["adrien", 10, 20, 30, 40, 6, 9, 33];
+
+console.log(student); // student sera egale à adrien
+console.log(marks); // affiche [10 , 20 , 30 , 40 , 6 , 9 , 33]
 
 // Fonctionne aussi avec les strings
