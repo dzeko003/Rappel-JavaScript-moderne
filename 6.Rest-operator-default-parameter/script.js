c2752fa2e0c7144e8757ff07c2f478b35c911101
dzeko003 <berenismassamba@gmail.com>
2026-01-29 10:38:14 +0100

    Correction de la mise en forme et ajout de points-virgules manquants dans les fonctions de concaténation et d'accueil.

--- 6.Rest-operator-default-parameter/script.js
+++ 6.Rest-operator-default-parameter/script.js
@@ -3,14 +3,12 @@
     Les paramètres du reste permettent d'accepter un nombre indéfini d'arguments rendus disponibles sous forme d'un tableau.
 */
 
-function concatenation(...strings){
-  console.log(strings);
-  return strings.reduce((acc,cur) => acc + cur)
+function concatenation(...strings) {
+  console.log(strings); // Affiche un tableau de chaine ["je " , "suis ", "heureux."]
+  return strings.reduce((acc, cur) => acc + cur);
 }
 
 console.log(concatenation("Je ", "suis ", "heureux."));
-
-
 
 /* 
     2. Les paramètres par défaut.
@@ -18,10 +16,9 @@ console.log(concatenation("Je ", "suis ", "heureux."));
     Ils permettent de définir une valeur par défaut si un argument correspondant au paramètre en question n'est pas fourni.
 */
 
-function welcoming(userName = "visiteur"){
-  return `Bienvenue, ${userName}.`
+function welcoming(userName = "visiteur") {
+  return `Bienvenue, ${userName}.`;
 }
 console.log(welcoming("Paul"));
 console.log(welcoming("Sara"));
 console.log(welcoming());
-
